aa9d01c6cfbf3e4947a0b20f950f0671298054ff
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-24 17:44:56 +0900

    회원 엔티티 주소와 목록추가

--- src/main/java/shop/shoppingmall/domain/Member.java
+++ src/main/java/shop/shoppingmall/domain/Member.java
@@ -1,16 +1,28 @@
 package shop.shoppingmall.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+import org.springframework.core.annotation.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails.*;
 
 @Entity
 @Getter @Setter
 public class Member {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue // 시퀀스 값을 사용하기 위함
+    @Column(name = "member_id") // Fk(외래키)가 MEMBER_ID
     private Long id;
-    private String username;
-}
+
+    private String name;
+
+    @Embedded // 내장 타입을 포함했다.
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
