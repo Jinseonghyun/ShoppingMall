82d92ad008d2ad14aecc1d999f75686425a8abaa
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-29 13:09:42 +0900

    회원 리포지토리 개발

--- src/main/java/shop/shoppingmall/repository/MemberRepository.java
+++ src/main/java/shop/shoppingmall/repository/MemberRepository.java
@@ -5,18 +5,30 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import shop.shoppingmall.domain.Member;
 
+import java.util.List;
+
 @Repository
 public class MemberRepository {
 
     @PersistenceContext
     EntityManager em;
 
-    public Long save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    // 단건 조회
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    // 이름으로 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList();
     }
 }
