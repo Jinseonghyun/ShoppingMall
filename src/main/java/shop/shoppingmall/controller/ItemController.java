b9c23aeb4c1bbda9ec461c9cf3c2dc4ff05b14ad
Jinseonghyun <jinsunghyun9@gmail.com>
2024-07-01 11:24:38 +0900

    상품 수정

--- src/main/java/shop/shoppingmall/controller/ItemController.java
+++ src/main/java/shop/shoppingmall/controller/ItemController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.shoppingmall.Service.ItemService;
 import shop.shoppingmall.domain.item.Book;
@@ -47,5 +49,39 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    /**
+     * 상품 수정
+     */
+    @GetMapping("items/{itemId}/edit") // {itemId} path variable 사용한다 -> 상품마다 달라지는 변형에 따라가야 하기 때문
+    public String updataItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId); // 원래 Item 타입이지만 단순하게 하기 위해 Book 으로 캐스팅
+
+        BookForm form = new BookForm(); // 업데이트 할때 Entity 가 아니라 Bookform 을 보낼거다.
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
